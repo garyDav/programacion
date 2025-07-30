9da47eeb821b8f6345b302dcdf25d96c385d7bb7
garyDav <gary.2810.dav@gmail.com>
2025-07-29 21:36:59 -0400

    Ejercicio Condicional

--- ejercicios/tema-03/parImparCero.cpp
+++ ejercicios/tema-03/parImparCero.cpp
@@ -1,20 +1,41 @@
-#include <iostream>
+#include<cmath>
+#include<iostream>
 using namespace std;
 
+int leerNumero();
+int parImparCero(int num);
+void mostrarSiPar(int res);
+
+int main() {
+  // Declaración de variables
+  int num, res;
+
+  // Asignación de variables
+  num = leerNumero();
+  res = parImparCero(num);
+  mostrarSiPar(res);
+
+  return EXIT_SUCCESS;
+}
+
 int leerNumero() {
-  double num;
+  double num=0;
   cout.width(15);
-  cout<<"Número: ";
+  cout<<"Número? ";
   cin>>num;
+  if (!num) {
+    cout<<"Error: No es un número."<<endl;
+    exit(EXIT_FAILURE);
+  }
   if (fmod(num, 1)) {
     cout<<"Error: El número debe ser un entero."<<endl;
-    exit(EXIT_FAILURE);
+    // exit(EXIT_FAILURE);
+    leerNumero();
   }
 
   return (int)num;
 }
 
-// Retorna 0, si num es cero, 1 si es impar y 2 si es par
 int parImparCero(int num) {
   if (!num) return 0;
 
@@ -37,16 +58,9 @@ void mostrarSiPar(int res) {
     case 2:
       cout<<"par."<<endl;
       break;
+    default:
+      cout<<"No introduciste un número."<<endl;
+      break;
   }
-}
-
-int main() {
-  int num, res;
-
-  num = leerNumero();
-  res = parImparCero(num);
-  mostrarSiPar(res);
-
-  return EXIT_SUCCESS;
 }
 
